c6002c442f99dddb25ab5ad82365f3cc38b10906
Habib <mahmoud.habib@truecaller.com>
2022-06-08 10:59:54 +0200

    Add Photos search common view model

---
+++ shared/src/commonMain/kotlin/com/bigo/flickrsearch/photos/presentation/PhotosSearchViewModel.kt
@@ -0,0 +1,48 @@
+package com.bigo.flickrsearch.photos.presentation
+
+import com.bigo.flickrsearch.core.coroutines.CoroutineContextProvider
+import com.bigo.flickrsearch.core.presentation.CommonViewModel
+import com.bigo.flickrsearch.photos.domain.SearchPhotos
+import com.bigo.flickrsearch.photos.domain.SearchQuery
+import com.bigo.flickrsearch.photos.domain.entities.PhotosPage
+
+data class PhotosSearchState(
+    val loading: Boolean? = null,
+    val photosPage: PhotosPage? = null,
+    val error: String? = null,
+) {
+    companion object {
+        fun initial() = PhotosSearchState()
+    }
+}
+
+class PhotosSearchViewModel(
+    coroutineContextProvider: CoroutineContextProvider,
+    private val searchPhotos: SearchPhotos,
+) : CommonViewModel<PhotosSearchState>(
+    PhotosSearchState.initial(),
+    coroutineContextProvider,
+) {
+    fun search(tag: String) {
+        launchBackground {
+            emit(state.copy(loading = true))
+            val photosSearchResult = searchPhotos(SearchQuery(listOf(tag)))
+            val newState =
+                photosSearchResult.fold(
+                    onSuccess = { value ->
+                        state.copy(
+                            photosPage = value,
+                            loading = false,
+                            error = null,
+                        )
+                    },
+                    onFailure = { error ->
+                        state.copy(
+                            error = error.message ?: "Something went wrong",
+                            loading = false,
+                        )
+                    })
+            emit(newState)
+        }
+    }
+}
